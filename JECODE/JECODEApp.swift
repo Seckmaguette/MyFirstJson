d56e5e9ce464784ddac01cf8ca41990c40e59460
maguette <seckmaguettte036@gmail.com>
2023-01-23 16:09:23 +0000

    Initial Commit

---
+++ JECODE/JECODEApp.swift
@@ -0,0 +1,17 @@
+//
+//  JECODEApp.swift
+//  JECODE
+//
+//  Created by Maguette SECK on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct JECODEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
